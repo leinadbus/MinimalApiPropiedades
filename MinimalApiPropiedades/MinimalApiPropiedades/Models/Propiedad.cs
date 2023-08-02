4b5d178fb9ede5f84029adc02f00070648e9b334
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-08-02 17:26:15 +0200

    Implementado Modelo Propiedad

---
+++ MinimalApiPropiedades/MinimalApiPropiedades/Models/Propiedad.cs
@@ -0,0 +1,15 @@
+﻿namespace MinimalApiPropiedades.Models
+{
+    public class Propiedad
+    {
+        public int IdPropiedad { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Ubicacion { get; set; }
+        public bool Activa { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+
+
+
+    }
+}
