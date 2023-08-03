58d32230fb78020c41ca9dd013b70d648cac5533
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-08-03 16:15:25 +0200

    Implementado Modelo CrearPropiedadDto

---
+++ MinimalApiPropiedades/MinimalApiPropiedades/Models/DTOS/CrearPropiedadDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalApiPropiedades.Models.DTOS
+{
+    public class CrearPropiedadDto
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Ubicacion { get; set; }
+        public bool Activa { get; set; }
+    }
+}
