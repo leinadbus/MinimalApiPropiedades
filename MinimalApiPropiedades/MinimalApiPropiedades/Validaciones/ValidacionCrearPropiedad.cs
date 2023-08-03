317264a19c63d1803d12753333a4e1637cfd0460
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-08-03 16:29:54 +0200

    Implementado Modelo validación ValidacionCrearPropiedad con FluentValidation

---
+++ MinimalApiPropiedades/MinimalApiPropiedades/Validaciones/ValidacionCrearPropiedad.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using MinimalApiPropiedades.Models.DTOS;
+
+namespace MinimalApiPropiedades.Validaciones
+{
+    public class ValidacionCrearPropiedad : AbstractValidator<CrearPropiedadDto>
+    {
+        public ValidacionCrearPropiedad()
+        {
+            RuleFor(modelo => modelo.Nombre).NotEmpty();
+            RuleFor(modelo => modelo.Descripcion).NotEmpty();
+            RuleFor(modelo => modelo.Ubicacion).NotEmpty();
+
+        }
+    }
+}
