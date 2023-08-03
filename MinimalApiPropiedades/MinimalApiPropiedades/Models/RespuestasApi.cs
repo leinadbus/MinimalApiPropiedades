acc9ba320f17022c14cf7dc14c5c304ecef75717
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-08-03 16:48:21 +0200

    Implementado Modelo RespuestasApi

---
+++ MinimalApiPropiedades/MinimalApiPropiedades/Models/RespuestasApi.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace MinimalApiPropiedades.Models
+{
+    public class RespuestasApi
+    {
+        public RespuestasApi()
+        {
+            Errores = new List<string>();
+        }
+        public bool Success { get; set; }
+        public Object Resultado { get; set; }
+        public HttpStatusCode codigoEstado { get; set; }
+        public List<string> Errores { get; set; }
+    }
+}
