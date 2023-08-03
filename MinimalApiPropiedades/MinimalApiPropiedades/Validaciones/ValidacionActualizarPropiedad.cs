7d31b27dbee17d1135f198b641eb20ca61ea1f0d
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-08-03 17:11:08 +0200

    Implementadas validaciones ValidacionActualizarPropiedad

---
+++ MinimalApiPropiedades/MinimalApiPropiedades/Validaciones/ValidacionActualizarPropiedad.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using MinimalApiPropiedades.Models.DTOS;
+
+namespace MinimalApiPropiedades.Validaciones
+{
+    public class ValidacionActualizarPropiedad : AbstractValidator<ActualizarPropiedadDto>
+    {
+        public ValidacionActualizarPropiedad()
+        {
+            RuleFor(modelo => modelo.IdPropiedad).NotEmpty().GreaterThan(0);
+            RuleFor(modelo => modelo.Nombre).NotEmpty();
+            RuleFor(modelo => modelo.Descripcion).NotEmpty();
+            RuleFor(modelo => modelo.Ubicacion).NotEmpty();
+
+        }
+    }
+}
