2ae371618bbc3ef26a57dac174767013156d79f0
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-08-03 17:10:44 +0200

    Implementado Modelo ActualizarPropiedadDto

---
+++ MinimalApiPropiedades/MinimalApiPropiedades/Models/DTOS/ActualizarPropiedadDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MinimalApiPropiedades.Models.DTOS
+{
+    public class ActualizarPropiedadDto
+    {
+        public int IdPropiedad { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Ubicacion { get; set; }
+        public bool Activa { get; set; }
+    }
+}
