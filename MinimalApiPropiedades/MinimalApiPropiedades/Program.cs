31ac975ecba58a0107b0c4a296b610c03c054bdc
Daniel Rodriguez <danielrodriguezsuarezbustillo@gmail.com>
2023-08-03 16:48:40 +0200

    Estandarizadas las respuestas de la API

--- MinimalApiPropiedades/MinimalApiPropiedades/Program.cs
+++ MinimalApiPropiedades/MinimalApiPropiedades/Program.cs
@@ -5,6 +5,7 @@ using MinimalApiPropiedades.Data;
 using MinimalApiPropiedades.Mapper;
 using MinimalApiPropiedades.Models;
 using MinimalApiPropiedades.Models.DTOS;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,74 +29,79 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+
 //Primeros EndPoints
 //Obtener todas las propiedades - GET - MapGet
 app.MapGet("/api/propiedades", (ILogger<Program> logger) =>
 {
+    RespuestasApi respuesta = new();
     //Usar el logger como ejemplo de inyección de dependencias
     logger.Log(LogLevel.Information, "Carga todas las propiedades");
 
-    return Results.Ok(DatosPropiedad.listaPropiedades);
-}).WithName("ObtenerPorpiedades").Produces<IEnumerable<Propiedad>>(200); 
+    respuesta.Resultado = DatosPropiedad.listaPropiedades;
+    respuesta.Success = true;
+    respuesta.codigoEstado = HttpStatusCode.OK;
+
+    return Results.Ok(respuesta);
+
+}).WithName("ObtenerPorpiedades").Produces<IEnumerable<RespuestasApi>>(200); 
+
+
 
 //Obtener propiedad individual - GET - MapGet
 app.MapGet("/api/propiedades/{id:int}", (int id) =>
 {
-    return Results.Ok(DatosPropiedad.listaPropiedades.FirstOrDefault(p => p.IdPropiedad == id));
-}).WithName("ObtenerPorpiedad").Produces<Propiedad>(200);
+    RespuestasApi respuesta = new();
+
+    respuesta.Resultado = DatosPropiedad.listaPropiedades.FirstOrDefault(p => p.IdPropiedad == id);
+    respuesta.Success = true;
+    respuesta.codigoEstado = HttpStatusCode.OK;
+
+    return Results.Ok(respuesta);
+
+}).WithName("ObtenerPorpiedad").Produces<RespuestasApi>(200);
+
+
 
 //Crear propiedad 
 app.MapPost("/api/crearPropiedad", (IMapper _mapper, 
     IValidator<CrearPropiedadDto> _validacion , [FromBody] CrearPropiedadDto crearPropiedadDto) =>
 {
 
+    RespuestasApi respuesta = new() { Success = false, codigoEstado = HttpStatusCode.BadRequest };
+
     var resultadoValidaciones = _validacion.ValidateAsync(crearPropiedadDto).GetAwaiter().GetResult();
 
     //Validar id de propiedad y nombre no null
     if(!resultadoValidaciones.IsValid)
     {
-        return Results.BadRequest(resultadoValidaciones.Errors.FirstOrDefault().ToString());
+        respuesta.Errores.Add(resultadoValidaciones.Errors.FirstOrDefault().ToString());
+        return Results.BadRequest(respuesta);
     }
     //Validar si el nombre ya existe
     if(DatosPropiedad.listaPropiedades.FirstOrDefault(p => p.Nombre.ToLower() == crearPropiedadDto.Nombre.ToLower()) != null)
     {
-        return Results.BadRequest("El nombre ya está registrado");
+        respuesta.Errores.Add("El nombre ya está registrado");
+        return Results.BadRequest(respuesta);
     }
 
-    //Propiedad propiedad = new Propiedad()
-    //{
-    //    Nombre = crearPropiedadDto.Nombre,
-    //    Descripcion = crearPropiedadDto.Descripcion,
-    //    Ubicacion = crearPropiedadDto.Ubicacion,
-    //    Activa = crearPropiedadDto.Activa
-    //};
-    //Esta linea de codigo hace lo que las anteriores
     Propiedad propiedad =_mapper.Map<Propiedad>(crearPropiedadDto);
-
 
     propiedad.IdPropiedad = DatosPropiedad.listaPropiedades.OrderByDescending(p => p.IdPropiedad).FirstOrDefault().IdPropiedad +1;
     DatosPropiedad.listaPropiedades.Add(propiedad);
 
-    //return Results.Ok(DatosPropiedad.listaPropiedades);
-
-    //return Results.Created($"/api/propiedades/{propiedad.IdPropiedad}", propiedad);
-
-
-    //PropiedadDto propiedadDto = new ()
-    //{
-    //    IdPropiedad = propiedad.IdPropiedad,
-    //    Nombre = propiedad.Nombre,
-    //    Descripcion = propiedad.Descripcion,
-    //    Ubicacion = propiedad.Ubicacion,
-    //    Activa = propiedad.Activa
-    //};
-    //Esta linea de codigo hace lo que las anteriores
-
     PropiedadDto propiedadDto = _mapper.Map<PropiedadDto>(propiedad);
 
-    return Results.CreatedAtRoute($"ObtenerPorpiedad", new { id = propiedad.IdPropiedad}, propiedadDto);
+    //return Results.CreatedAtRoute($"ObtenerPorpiedad", new { id = propiedad.IdPropiedad}, propiedadDto);
 
-}).WithName("CrearPorpiedad").Accepts<CrearPropiedadDto>("application/json").Produces<PropiedadDto>(201).Produces(400); 
+    respuesta.Resultado = propiedadDto;
+    respuesta.Success = true;
+    respuesta.codigoEstado = HttpStatusCode.Created;
+
+    return Results.Ok(respuesta);
+
+}).WithName("CrearPorpiedad").Accepts<CrearPropiedadDto>("application/json").Produces<RespuestasApi>(201).Produces(400); 
 
 app.UseHttpsRedirection();
 
